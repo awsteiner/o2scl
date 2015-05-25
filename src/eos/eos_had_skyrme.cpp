b26f1ca65e7170d7443b4656169848e497d5b3f5
Andrew W. Steiner <awsteiner@mykolab.com>
2015-05-25 12:26:32 -0400

    [dev] Added a bit of debug info in eos_had_skyrme.

--- src/eos/eos_had_skyrme.cpp
+++ src/eos/eos_had_skyrme.cpp
@@ -256,9 +256,9 @@ int eos_had_skyrme::calc_e(fermion &ne, fermion &pr, thermo &locth) {
   pr.ms=pr.m/(1.0+2.0*(n*term+pr.n*term2)*pr.m);
 
   if (ne.ms<0.0 || pr.ms<0.0) {
-    cout << ne.n << " " << pr.n << endl;
-    cout << term << " " << term2 << endl;
-    cout << ne.ms << " " << pr.ms << endl;
+    cout << "nn,np: " << ne.n << " " << pr.n << endl;
+    cout << "term,term2: " << term << " " << term2 << endl;
+    cout << "msn,msp: " << ne.ms << " " << pr.ms << endl;
     O2SCL_ERR2("Effective masses negative in ",
 	       "eos_had_skyrme::calc_temp_e().",exc_einval);
   }
