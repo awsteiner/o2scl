f724abd4729468195ef154f508ff6d1117bb950a
awsteiner <awsteiner@mykolab.com>
2016-08-09 16:36:35 -0400

    Finished cloud_file with legacy I/O.

--- src/hdf/cloud_file.h
+++ src/hdf/cloud_file.h
@@ -29,7 +29,7 @@
 #include <iostream>
 // For getenv() 
 #include <cstdlib>
-// For struct stat
+// For struct stat and associated functions
 #include <sys/stat.h>
 #include <o2scl/err_hnd.h>
 #include <o2scl/hdf_file.h>
@@ -39,65 +39,48 @@
 namespace o2scl_hdf {
 #endif
 
-  /** \brief Desc
-
+  /** \brief Read a file and download from a URL if necessary
+      
       \note This function requires POSIX I/O calls and a system call
       which uses <tt>mkdir -p</tt>, thus will probably only work on
       unix-like systems.
 
       \todo Convert to use boost::filesystem .
   */
-  template<class data_t=int> class cloud_file {
-
-  public:
+  class cloud_file {
     
+  public:
+  
     /** \brief If true, allow the use of \c wget to download the file
      */
     bool allow_wget;
-    /** \brief Desc
+    /** \brief If true, allow the use of \c curl to download the file
      */
     bool allow_curl;
-    /** \brief Desc
+    /** \brief (currently unused)
      */
     bool force_subdir;
-    /** \brief Desc
+    /** \brief Verbosity parameter
      */
     int verbose;
-    /** \brief Desc
+    /** \brief If true, throw an exception on failure
      */
     bool throw_on_fail;
-    /** \brief Desc
+    /** \brief The environment variable which stores the directory
      */
     std::string env_var;
+  
+    cloud_file();
     
-    cloud_file() {
-      allow_wget=true;
-      allow_curl=true;
-      force_subdir=true;
-      verbose=1;
-      throw_on_fail=true;
-      env_var="O2SCL_DATA";
-    }
-    
-    /** \brief Desc
-     */
+    /** \brief Open an HDF file named \c file in directory \c dir
+	in subdirectory \c subdir, downloading from URL \c url if
+	necessary
+    */
     int hdf5_open(hdf_file &hf, std::string file, std::string subdir,
-		  std::string url, std::string dir="") {
-      get_file(file,subdir,url,dir);
-      hf.open(file);
-      return 0;
-    }
+		  std::string url, std::string dir="");
     
-    /** \brief Desc
-     */
-    int hdf5_open_or_create(hdf_file &hf, std::string file, std::string subdir,
-			    std::string url, std::string dir="") {
-      get_file(file,subdir,url,dir);
-      hf.open_or_create(file);
-      return 0;
-    }
-    
-    /** \brief Desc
+    /** \brief Get file named \c file in directory \c dir 
+	in subdirectory \c subdir from url \c url
 	
 	This function attempts to find a file named \c file in
 	subdirectory \c subdir of the data directory \c dir. If \c dir
@@ -110,104 +93,204 @@ namespace o2scl_hdf {
 	downloading the file, then the full filename is returned.
 	Otherwise, an exception is thrown.
     */
-    int get_file(std::string &file, std::string subdir, std::string url,
-			 std::string dir="") {
-
-      if (dir=="") {
-	char *dir_ptr=getenv(env_var.c_str());
-	if (dir_ptr!=0) {
-	  dir=dir_ptr;
-	}
-	if (verbose>1) {
-	  std::cout << "Obtained directory from environment variable "
-		    << env_var << ": " << dir << std::endl;
-	}
-      }
-
-      std::cout << "Here6 " << dir << std::endl;
-      boost::filesystem::path p(dir.c_str());
-      std::cout << "Here7." << std::endl;
-      std::cout << boost::filesystem::exists(p) << std::endl;
-      std::cout << "Here7b." << std::endl;
-      std::cout << boost::filesystem::is_directory(p) << std::endl;
-      std::cout << "Here8." << std::endl;
-      std::cout << boost::filesystem::is_regular_file(p) << std::endl;
-      std::cout << "Here9." << std::endl;
-      exit(-1);
-      
-      struct stat sb;
-      if (dir.length()==0 || stat(dir.c_str(),&sb)!=0 ||
-	  !S_ISDIR(sb.st_mode)) {
-	std::cout << "No directory specified. Please enter directory."
-		  << std::endl;
-	std::cin >> dir;
-	if (dir.length()==0 || stat(dir.c_str(),&sb)!=0 ||
-	    !S_ISDIR(sb.st_mode)) {
-	  if (throw_on_fail) {
-	    O2SCL_ERR("Could not find correct directory.",
-		      o2scl::exc_efilenotfound);
-	  } else {
-	    return o2scl::exc_efilenotfound;
-	  }
-	}
-      }
-
-      std::string full_dir=dir+"/"+subdir;
-      bool full_dir_present=true;
-      
-      if (full_dir.length()==0 || stat(full_dir.c_str(),&sb)!=0 ||
-	  !S_ISDIR(sb.st_mode)) {
-	if (verbose>0) {
-	  std::cout << "Directory did not exist. Trying mkdir."
-		    << std::endl;
-	}
-	std::string cmd=((std::string)"mkdir -p ")+full_dir;
-	int ret=system(cmd.c_str());
-	if (ret!=0) {
-	  if (throw_on_fail) {
-	    O2SCL_ERR("Failed to create directory.",o2scl::exc_efilenotfound);
-	  } else {
-	    return o2scl::exc_efilenotfound;
-	  }
-	}
-	full_dir_present=false;
-      }
-      
-      std::string fname=full_dir+"/"+file;
-      if (!full_dir_present || stat(fname.c_str(),&sb)!=0 ||
-	  !S_ISREG(sb.st_mode)) {
-	int ret=1;
-	if (allow_curl) {
-	  if (verbose>0) {
-	    std::cout << "Directory did not exist. Trying curl."
-		      << std::endl;
-	  }
-	  std::string cmd=((std::string)"cd ")+full_dir+"; curl "+url;
-	  ret=system(cmd.c_str());
-	}
-	if (allow_wget && ret!=0) {
-	  if (verbose>0) {
-	    std::cout << "Directory did not exist. Trying wget."
-		      << std::endl;
-	  }
-	  std::string cmd=((std::string)"cd ")+full_dir+"; wget "+url;
-	  ret=system(cmd.c_str());
-	}
-	if (ret!=0) {
-	  if (throw_on_fail) {
-	    O2SCL_ERR("Failed to download file.",o2scl::exc_efilenotfound);
-	  } else {
-	    return o2scl::exc_efilenotfound;
-	  }
-	}
-      }
-
-      file=fname;
-      return o2scl::success;
-    }
-    
+    int get_file(std::string file, std::string subdir, std::string url,
+		 std::string &fname, std::string dir="");
+  
   };
 
+  cloud_file::cloud_file() {
+    allow_wget=true;
+    allow_curl=true;
+    force_subdir=true;
+    verbose=1;
+    throw_on_fail=true;
+    env_var="";
+  }
+
+  int cloud_file::hdf5_open(hdf_file &hf, std::string file,
+			    std::string subdir, std::string url,
+			    std::string dir) {
+    std::string fname;
+    get_file(file,subdir,url,fname,dir);
+    hf.open(fname);
+    return 0;
+  }
+    
+  int cloud_file::get_file(std::string file, std::string subdir,
+			   std::string url, std::string &fname,
+			   std::string dir) {
+
+    if (dir=="" && env_var.length()>0) {
+      char *dir_ptr=getenv(env_var.c_str());
+      if (dir_ptr!=0) {
+	dir=dir_ptr;
+      }
+      if (verbose>1) {
+	std::cout << "Obtained directory from environment variable '"
+		  << env_var << "':\n\t" << dir << std::endl;
+      }
+    }
+
+#ifdef O2SCL_LEGACY_IO
+
+    // File status object
+    struct stat sb;
+    // Return value of stat()
+    int sret=1;
+
+    // Look for directory 
+    bool dir_present=false;
+    if (dir.length()>0) {
+      sret=stat(dir.c_str(),&sb);
+      if (sret==0) {
+	dir_present=S_ISDIR(sb.st_mode);
+      }
+    }
+    if (dir.length()==0 || sret!=0 || dir_present==false) {
+      // If not found, prompt user for it
+      std::cout << "No directory specified. Please enter directory."
+		<< std::endl;
+      std::cin >> dir;
+      // Check again
+      dir_present=false;
+      if (dir.length()>0) {
+	sret=stat(dir.c_str(),&sb);
+	if (sret==0) {
+	  dir_present=S_ISDIR(sb.st_mode);
+	}
+      }
+      // If that failed, then give up
+      if (dir.length()==0 || sret!=0 || dir_present==false) {
+	if (throw_on_fail) {
+	  O2SCL_ERR("Could not find correct directory.",
+		    o2scl::exc_efilenotfound);
+	} else {
+	  return o2scl::exc_efilenotfound;
+	}
+      }
+    } else if (verbose>1) {
+      std::cout << "Directory " << dir << " found." << std::endl;
+    }
+
+    // Now look for subdirectory
+    std::string full_dir=dir+"/"+subdir;
+    if (verbose>1) {
+      std::cout << "Set full_dir to: " << full_dir << std::endl;
+    }
+    bool full_dir_present=false;
+    sret=stat(full_dir.c_str(),&sb);
+    if (sret==0) {
+      full_dir_present=S_ISDIR(sb.st_mode);
+    }
+      
+    if (full_dir.length()==0 || sret!=0 || full_dir_present==false) {
+      // If not found, try to make it with 'mkdir'
+      std::string cmd=((std::string)"mkdir -p ")+full_dir;
+      if (verbose>0) {
+	std::cout << "Directory did not exist. Trying mkdir with "
+		  << "command:\n\t" << cmd << std::endl;
+      }
+      int ret=system(cmd.c_str());
+      if (ret!=0) {
+	if (throw_on_fail) {
+	  O2SCL_ERR("Failed to create directory.",o2scl::exc_efilenotfound);
+	} else {
+	  return o2scl::exc_efilenotfound;
+	}
+      }
+      // Check again
+      full_dir_present=false;
+      sret=stat(full_dir.c_str(),&sb);
+      if (sret==0) {
+	full_dir_present=S_ISDIR(sb.st_mode);
+      }
+      // If that failed, then give up
+      if (full_dir.length()==0 || sret!=0 || full_dir_present==false) {
+	if (throw_on_fail) {
+	  O2SCL_ERR("Could not create full directory.",
+		    o2scl::exc_efilenotfound);
+	} else {
+	  return o2scl::exc_efilenotfound;
+	}
+      }
+    } else if (verbose>1) {
+      std::cout << "Full directory " << full_dir << " found." << std::endl;
+    }
+
+    // Now look for the full data file
+    fname=full_dir+"/"+file;
+    bool file_present=false;
+    sret=stat(fname.c_str(),&sb);
+    if (sret==0) {
+      file_present=S_ISREG(sb.st_mode);
+    }
+      
+    if (sret!=0 || file_present==false) {
+      // If it couldn't be found, try to download it
+      int ret=1;
+      if (allow_curl) {
+	std::string cmd=((std::string)"cd ")+full_dir+"; curl "+url;
+	if (verbose>0) {
+	  std::cout << "File did not exist. Trying curl command:\n\t"
+		    << cmd << std::endl;
+	}
+	ret=system(cmd.c_str());
+      }
+      if (allow_wget && ret!=0) {
+	std::string cmd=((std::string)"cd ")+full_dir+"; wget "+url;
+	if (verbose>0) {
+	  std::cout << "File did not exist. Trying wget command:\n\t"
+		    << cmd << std::endl;
+	}
+	ret=system(cmd.c_str());
+      }
+      // Check to see if download command succeeded
+      if (ret!=0) {
+	if (throw_on_fail) {
+	  O2SCL_ERR("Failed to download file.",o2scl::exc_efilenotfound);
+	} else {
+	  return o2scl::exc_efilenotfound;
+	}
+      }
+      // Check to see if the file is there
+      sret=stat(fname.c_str(),&sb);
+      if (sret==0) {
+	file_present=S_ISREG(sb.st_mode);
+      }
+      // If it's still not there, then give up
+      if (sret!=0 || file_present==false) {
+	if (throw_on_fail) {
+	  O2SCL_ERR("Could not find or download file.",
+		    o2scl::exc_efilenotfound);
+	} else {
+	  return o2scl::exc_efilenotfound;
+	}
+      }
+    }
+
+    // Output full filename
+    if (verbose>1) {
+      std::cout << "Success with file named " << fname << std::endl;
+    }
+
+#else
+
+    std::cout << "Here6 " << dir << std::endl;
+    boost::filesystem::path p(dir.c_str());
+    std::cout << "Here7." << std::endl;
+    std::cout << boost::filesystem::exists(p) << std::endl;
+    std::cout << "Here7b." << std::endl;
+    std::cout << boost::filesystem::is_directory(p) << std::endl;
+    std::cout << "Here8." << std::endl;
+    std::cout << boost::filesystem::is_regular_file(p) << std::endl;
+    std::cout << "Here9." << std::endl;
+    exit(-1);
+      
+#endif      
+      
+    return o2scl::success;
+  }
+  
 #ifndef DOXYGEN_NO_O2NS
 }
 #endif
