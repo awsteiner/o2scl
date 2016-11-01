2dc82613f43842b7afde324012ab6f99c00ebfcc
Andrew W. Steiner <awsteiner@mykolab.com>
2016-11-01 15:39:57 -0400

    Work on setup for o2graph in o2sclpy.

--- python/setup.py
+++ python/setup.py
@@ -6,4 +6,10 @@ setup(name='o2sclpy',version='0.920',
       author='Andrew W. Steiner',
       author_email='awsteiner@mykolab.com',license='GPLv3',
       packages=['o2sclpy'],install_requires=['h5py','numpy','matplotlib'],
-      zip_safe=False,scripts=['bin/o2graph'])
+      zip_safe=False,scripts=['bin/o2graph'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+          'Programming Language :: Python :: 3 :: Only',
+          'Topic :: Scientific/Engineering'
+      ])
