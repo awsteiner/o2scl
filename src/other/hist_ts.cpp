11a16fdf2f56d5781a6d1fe1ba5567bba8720ac6
Andrew W. Steiner <awsteiner@mykolab.com>
2017-01-26 13:07:57 -0500

    Fixing travis.

--- src/other/hist_ts.cpp
+++ src/other/hist_ts.cpp
@@ -71,7 +71,7 @@ int main(void) {
   for(size_t i=0;i<10;i++) {
     cout << i << " " << h2.get_rep_i(i) << " " << h2[i] << endl;
   }
-  cout << h2.bin_sum() << endl;
+  cout << h2.sum_wgts() << endl;
   
   t.report();
   return 0;
