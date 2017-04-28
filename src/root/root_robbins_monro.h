88a71be95cb111fd5c2b938ef4d98d0060851d85
Andrew W. Steiner <awsteiner@mykolab.com>
2017-04-28 12:08:17 -0400

    Added robbins monro iteration.

---
+++ src/root/root_robbins_monro.h
@@ -0,0 +1,167 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2017, Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+#ifndef O2SCL_ROOT_ROBBINS_MONRO_H
+#define O2SCL_ROOT_ROBBINS_MONRO_H
+
+/** \file root_robbins_monro.h
+    \brief File defining \ref o2scl::root_robbins_monro 
+*/
+
+#include <limits>
+#include <o2scl/funct.h>
+#include <o2scl/root.h>
+
+#ifndef DOXYGEN_NO_O2NS
+namespace o2scl {
+#endif
+  
+  /** \brief One-dimensional root-finding for noisy functions
+   */
+  template<class func_t=funct, class dfunc_t=func_t>
+    class root_robbins_monro : public root<func_t,dfunc_t> {
+    
+  public:
+  
+  root_robbins_monro() {
+    coeff=1.0;
+    gamma=1.0;
+  }
+
+  /// If true, apply Polyak-Ruppert averaging
+  bool pr_averaging;
+
+  /// Coefficient for sequence (default 1.0)
+  double coeff;
+
+  /// Exponent for sequence (default 1.0)
+  double gamma;
+  
+  /// Return the type, \c "root_robbins_monro".
+  virtual const char *type() { return "root_robbins_monro"; }
+
+  /** \brief Solve \c func using \c x as an initial guess
+   */
+  virtual int solve(double &x, func_t &func) {
+    double x2;
+
+    if (pr_averaging) {
+      
+      double avg=0.0;
+      for(size_t i=0;i<this->ntrial/2;i++) {
+	double fx=func(x);
+	if (fabs(fx)<this->tol_rel) {
+	  return o2scl::success;
+	}
+	x2=x-fx*coeff/pow(((double)(i+1)),gamma);
+	avg+=(x2*((double)i)+avg)/((double)(i+1));
+	double favg=func(avg);
+	if (fabs(favg)<this->tol_rel) {
+	  x=avg;
+	  return o2scl::success;
+	}
+	x=x2;
+      }
+      O2SCL_CONV2_RET("Function root_robbins_monro::solve() exceeded ",
+		      "maximum number of iterations.",o2scl::exc_emaxiter,
+		      this->err_nonconv);
+    } 
+
+    for(size_t i=0;i<this->ntrial;i++) {
+      double fx=func(x);
+      if (fabs(fx)<this->tol_rel) {
+	return o2scl::success;
+      }
+      x2=x-f(x)*coeff/pow(((double)(i+1)),gamma);
+      x=x2;
+    }
+    fx=func(x);
+    if (fabs(fx)<this->tol_rel) {
+      return o2scl::success;
+    }
+    O2SCL_CONV2_RET("Function root_robbins_monro::solve() exceeded ",
+		    "maximum number of iterations.",o2scl::exc_emaxiter,
+		    this->err_nonconv);
+    return o2scl::exc_emaxiter;
+  }
+  
+  /// Solve \c func in region \f$ x_1<x<x_2 \f$ returning \f$ x_1 \f$.
+  virtual int solve_bkt(double &x1, double x2, func_t &f) {
+    x1=(x1+x2)/2.0;
+    return solve(x1,f);
+  }
+
+  /** \brief Solve \c func using \c x as an initial
+      guess using derivatives \c df.
+  */
+  virtual int solve_de(double &x, func_t &func, dfunc_t &df) {
+    double x2;
+
+    if (pr_averaging) {
+      
+      double avg=0.0;
+      for(size_t i=0;i<this->ntrial/2;i++) {
+	double fx=func(x);
+	double fpx=df(x);
+	if (fabs(fx)<this->tol_rel) {
+	  return o2scl::success;
+	}
+	x2=x-fx/df*coeff/pow(((double)(i+1)),gamma);
+	avg+=(x2*((double)i)+avg)/((double)(i+1));
+	double favg=func(avg);
+	if (fabs(favg)<this->tol_rel) {
+	  x=avg;
+	  return o2scl::success;
+	}
+	x=x2;
+      }
+      O2SCL_CONV2_RET("Function root_robbins_monro::solve() exceeded ",
+		      "maximum number of iterations.",o2scl::exc_emaxiter,
+		      this->err_nonconv);
+    } 
+
+    for(size_t i=0;i<this->ntrial;i++) {
+      double fx=func(x);
+      double fpx=df(x);
+      if (fabs(fx)<this->tol_rel) {
+	return o2scl::success;
+      }
+      x2=x-fx/df*coeff/pow(((double)(i+1)),gamma);
+      x=x2;
+    }
+    fx=func(x);
+    if (fabs(fx)<this->tol_rel) {
+      return o2scl::success;
+    }
+    O2SCL_CONV2_RET("Function root_robbins_monro::solve() exceeded ",
+		    "maximum number of iterations.",o2scl::exc_emaxiter,
+		    this->err_nonconv);
+    return o2scl::exc_emaxiter;
+  }
+  
+  };
+   
+#ifndef DOXYGEN_NO_O2NS
+}
+#endif
+
+#endif
---
+++ src/root/root_robbins_monro_ts.cpp
@@ -0,0 +1,135 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2006-2017, Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+#include <o2scl/funct.h>
+#include <o2scl/root_robbins_monro.h>
+#include <o2scl/test_mgr.h>
+
+using namespace std;
+using namespace o2scl;
+
+double gfn(double x) {
+  return sin((x-0.2)*4)*(1.0+sin((x-0.2)*50.0)/1.1);
+}
+
+double gfnp(double x) {
+  return tanh(100.0*(x-0.2));
+}
+
+int main(void) {
+
+  cout.setf(ios::scientific);
+  
+  test_mgr t;
+  t.set_output_level(2);
+
+  cl acl;
+  double a, b, r;
+
+  funct fmg1=gfn;
+  funct fmg2=gfn2;
+  funct_gsl fg1(fmg1);
+  funct_gsl fg2(fmg2);
+
+  for(size_t k=0;k<2;k++) {
+
+    // GSL 
+    {
+      int status;
+      int iter=0, max_iter=100;
+
+      a=-1.0;
+      b=1.0;
+    
+      const gsl_root_fsolver_type *T=gsl_root_fsolver_brent;
+      gsl_root_fsolver *s=gsl_root_fsolver_alloc (T);
+      
+      if (k==0) gsl_root_fsolver_set(s,&fg1,a,b);
+      else gsl_root_fsolver_set(s,&fg2,a,b);
+     
+      do {
+
+	iter++;
+	status=gsl_root_fsolver_iterate(s);
+	r=gsl_root_fsolver_root(s);
+	a=gsl_root_fsolver_x_lower(s);
+	b=gsl_root_fsolver_x_upper(s);
+	status=gsl_root_test_interval(a,b,0,1.0e-6);
+
+	cout << iter << " " << a << " " << r << " " << b << endl;
+
+      } while (status==gsl_continue && iter<max_iter);
+     
+      gsl_root_fsolver_free(s);
+
+      if (k==0) {
+	cout << r << " " << gfn(r) << endl;
+      } else {
+	cout << r << " " << gfn2(r) << endl;
+      }
+      t.test_rel(r,0.2,1.0e-10,"1");
+    }
+
+    // Using set() and iterate() with a function pointer
+    typedef double (*gfnt)(double);
+    root_robbins_monro<gfnt> grb1;
+    gfnt gfnv=&gfn;
+    if (k==1) gfnv=&gfn2;
+    a=-1.0;
+    b=1.0;
+    grb1.set(gfnv,a,b);
+    int status=gsl_continue;
+    for(size_t iter=0;iter<100 && status==gsl_continue;iter++) {
+
+      grb1.iterate(gfnv);
+
+      r=grb1.get_root();
+      a=grb1.get_lower();
+      b=grb1.get_upper();
+      status=gsl_root_test_interval(a,b,0,1.0e-6);
+
+      cout << iter << " " << a << " " << r << " " << b << endl;
+    }
+    
+    if (k==0) {
+      cout << r << " " << gfn(r) << endl;
+    } else {
+      cout << r << " " << gfn2(r) << endl;
+    }
+    t.test_rel(grb1.get_root(),0.2,1.0e-10,"1");
+
+  }
+
+  // Using a funct object and the solve_bkt() interface
+  funct fmf=std::bind(std::mem_fn<double(double)>
+			(&cl::mfn),&acl,std::placeholders::_1);
+  root_robbins_monro<> grb2;
+  a=-1.0;
+  b=1.0;
+  grb2.solve_bkt(a,b,fmf);
+  t.test_rel(a,0.2,1.0e-10,"1");
+  cout << a << " " << gfn(a) << endl;
+
+  t.report();
+  return 0;
+}
+
