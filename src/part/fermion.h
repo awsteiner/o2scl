01ddd897c21f5512aa759e294323627aec6df627
Andrew W. Steiner <awsteiner@mykolab.com>
2019-01-28 11:48:47 -0500

    Removing code duplication in fermion_rel <-> fermion_deriv_rel.

--- src/part/fermion.h
+++ src/part/fermion.h
@@ -303,7 +303,7 @@ namespace o2scl {
 
 	\note This function returns an integer value, in contrast to
 	\ref calc_mu(), because of the potential for non-convergence.
-     */
+    */
     virtual int calc_density(fermion &f, double temper)=0;
 
     /** \brief Calculate properties with antiparticles as function of
@@ -400,29 +400,261 @@ namespace o2scl {
     /// Return string denoting type ("fermion_thermo")
     virtual const char *type() { return "fermion_thermo"; }
 
-#ifdef O2SCL_NEVER_DEFINED
-    /** \brief Test the thermodynamics of calc_density(),
-	calc_mu(), pair_density() and pair_mu()
+    /** \brief Desc
+     */
+    template<class fermion_t>
+      bool calc_mu_deg_tlate(fermion_t &f, double temper, 
+			     double prec) {
+  
+      // Handle the zero-temperature limit
+      if (temper==0.0) {
+	calc_mu_zerot(f);
+	return true;
+      }
 
-	This compares the approximation to the exact results over a
-	grid with \f$ T = \left\{10^{-2},1,10^{2}\right\} \f$, \f$
-	\log_{10} (m/T) = \left\{ -3,-2,-1,0,1,2,3\right\} \f$, and
-	\f$ \log_{10} \psi = \left\{ -3,-2,-1,0,1\right\} \f$, where
-	\f$ \psi \equiv \left(\mu-m\right)/T \f$ using
-	calc_density() and calc_mu(), with both inc_rest_mass
-	taking both values <tt>true</tt> and <tt>false</tt>.
+      // Double check to ensure T and mass are positive
+      if (temper<0.0 || f.ms<0.0) {
+	O2SCL_ERR2("Temperature or mass negative in fermion_thermo",
+		   "::calc_mu_deg().",exc_einval);
+      }
+  
+      if (f.non_interacting==true) { f.nu=f.mu; f.ms=f.m; }
+  
+      // Compute psi and tt
+      double psi;
+      if (f.inc_rest_mass) psi=(f.nu-f.ms)/temper;
+      else psi=(f.nu+f.m-f.ms)/temper;
+      double tt=temper/f.ms;
 
-	The <tt>verbose</tt> parameter controls the amount of
-	output, and \c fname is the filename for the file
-	<tt>fermion_cal.o2</tt>.
+      // Return false immediately psi<0 where the expressions below
+      // don't work because of the square roots
+      if (psi<0.0) return false;
+  
+      // Prefactor 'd' in Johns96
+      double prefac=f.g/2.0/o2scl_const::pi2*pow(f.ms,4.0);
+  
+      // Define x = psi * t = (mu/m - 1) and related values
+      double x=psi*tt;
+      double sx=sqrt(x);
+      double s2x=sqrt(2.0+x);
+      double x2=x*x;
+      double x3=x2*x;
+      double x4=x2*x2;
+  
+      // Evaluate the first and last term for the pressure
+      double pterm1;
+      if (x>1.0e-5) {
+	pterm1=(x*(1.0+x)*(2.0+x)*(-3.0+2.0*x*(2.0+x))+6.0*sx*s2x*
+		log((sx+s2x)/sqrt(2.0)))/24.0/sx/s2x;
+      } else {
+	pterm1=x2*sx*(29568.0+15840.0*x+1540.0*x2-105.0*x3)/55440.0/sqrt(2.0);
+      }
+      double pterm4=-31.0*pow(o2scl_const::pi*tt,6.0)/1008.0*(1.0+x)*
+	sx*s2x/pow(x*(2.0+x),4.0);
 
-	\future Also calibrate massless fermions?
-	\future Convert into separate class?
-    */
-    virtual double calibrate(fermion &f, int verbose=0, bool test_pair=false,
-			     std::string fname="");
-#endif
+      // Check if we're going to succeed
+      if (fabs(pterm4)/fabs(pterm1)>prec) {
+	return false;
+      }
+  
+      // First order density term (first order entropy term is zero)
+      double nterm1=sx*s2x*x*(2.0+x)/3.0/f.ms;
+  
+      // Second order terms
+      double pterm2=tt*tt*o2scl_const::pi2/6.0*(1.0+x)*sx*s2x;
+      double nterm2=tt*tt*o2scl_const::pi2/6.0*(1.0+4.0*x+2.0*x2)/
+	f.ms/sx/s2x;
+      double enterm2=tt*o2scl_const::pi2/3.0*(1.0+x)*sx*s2x/f.ms;
 
+      // Third order terms
+      double pterm3=7.0*pow(o2scl_const::pi*tt,4.0)/360.0*(1.0+x)*
+	(-1.0+4.0*x+2.0*x2)/pow(x*(2.0+x),1.5);
+      double nterm3=7.0*pow(o2scl_const::pi*tt,4.0)/120.0/sx/s2x/
+	x2/(x+2.0)/(x+2.0)/f.ms;
+      double enterm3=7.0*pow(o2scl_const::pi*tt,4.0)/tt/90.0*(1.0+x)*
+	(-1.0+4.0*x+2.0*x2)/f.ms/sx/s2x/x/(x+2.0);
+
+      // Fourth order terms for density and entropy
+      double nterm4=31.0*pow(o2scl_const::pi*tt,6.0)/1008.0*sx*s2x*
+	(7.0+12.0*x+6.0*x2)/f.ms/pow(x*(2.0+x),5.0);
+      double enterm4=-31.0*pow(o2scl_const::pi*tt,6.0)/tt/168.0*sx*s2x*
+	(1.0+x)/pow(x*(2.0+x),4.0);
+
+      // Add up all the terms
+      f.pr=prefac*(pterm1+pterm2+pterm3+pterm4);
+      f.n=prefac*(nterm1+nterm2+nterm3+nterm4);
+      f.en=prefac*(enterm2+enterm3+enterm4);
+      f.ed=-f.pr+f.nu*f.n+temper*f.en;
+
+      return true;
+    }
+
+    /** \brief Desc
+     */
+    template<class fermion_t>
+      bool calc_mu_ndeg_tlate(fermion_t &f, double temper, 
+			      double prec, bool inc_antip) {
+  
+      if (f.non_interacting==true) { f.nu=f.mu; f.ms=f.m; }
+
+      // Compute psi and tt
+      double psi, psi_num;
+      if (f.inc_rest_mass) {
+	psi_num=f.nu-f.ms;
+      } else {
+	psi_num=f.nu+f.m-f.ms;
+      }
+      psi=psi_num/temper;
+      double tt=temper/f.ms;
+
+      // Return false immediately if we're degenerate
+      if (inc_antip==false && psi>-1.0) return false;
+
+      // Prefactor 'd' in Johns96
+      double prefac=f.g/2.0/o2scl_const::pi2*pow(f.ms,4.0);
+
+      // One term is always used, so only values of max_term greater than
+      // 0 are useful.
+      static const size_t max_term=200;
+  
+      // Maximum argument for exponential
+      // double log_dbl_max=709.78;
+
+      // Return zero if psi+1/t is too small
+      if (psi+1.0/tt<-700.0) {
+	f.n=0.0;
+	f.ed=0.0;
+	f.pr=0.0;
+	f.en=0.0;
+	return true;
+      }
+
+      // -----------------------------------------------------
+      // Return early if the last term is going to be too large.
+  
+      // Ratio of last term to first term in the pressure expansion
+      double rat;
+      double dj1=((double)max_term), jot1=max_term/tt;
+      double dj2=1.0, jot2=1.0/tt;
+      if (inc_antip==false) {
+	rat=exp(dj1*psi)/jot1/jot1*gsl_sf_bessel_Kn_scaled(2.0,jot1);
+	rat/=exp(dj2*psi)/jot2/jot2*gsl_sf_bessel_Kn_scaled(2.0,jot2);
+      } else {
+	if (f.inc_rest_mass) {
+	  rat=exp(-jot1)*2.0*cosh(dj1*f.nu/temper)/jot1/jot1*
+	    gsl_sf_bessel_Kn_scaled(2.0,jot1);
+	  rat/=exp(-jot2)*2.0*cosh(dj2*f.nu/temper)/jot2/jot2*
+	    gsl_sf_bessel_Kn_scaled(2.0,jot2);
+	} else {
+	  rat=exp(-jot1)*2.0*cosh(dj1*(f.nu+f.m)/temper)/jot1/jot1*
+	    gsl_sf_bessel_Kn_scaled(2.0,jot1);
+	  rat/=exp(-jot2)*2.0*cosh(dj2*(f.nu+f.m)/temper)/jot2/jot2*
+	    gsl_sf_bessel_Kn_scaled(2.0,jot2);
+	}
+      }
+
+      // If the ratio between the last term and the first term is 
+      // not small enough, return false
+      if (std::isfinite(rat) && rat>prec) {
+	return false;
+      }
+  
+      double first_term=0.0;
+      f.pr=0.0;
+      f.n=0.0;
+      f.en=0.0;
+
+      for(size_t j=1;j<=max_term;j++) {
+
+	double dj=((double)j);
+	double jot=dj/tt;
+
+	double pterm, nterm, enterm;
+
+	if (inc_antip==false) {
+	  pterm=exp(dj*psi)/jot/jot*gsl_sf_bessel_Kn_scaled(2.0,jot);
+	  if (j%2==0) {
+	    pterm*=-1.0;
+	  }
+	  nterm=pterm*dj/temper;
+	} else {
+	  if (f.inc_rest_mass) {
+	    pterm=exp(-jot)*2.0*cosh(dj*f.nu/temper)/jot/jot*
+	      gsl_sf_bessel_Kn_scaled(2.0,jot);
+	    if (j%2==0) {
+	      pterm*=-1.0;
+	    }
+	    nterm=pterm*tanh(dj*f.nu/temper)*dj/temper;
+	  } else {
+	    pterm=exp(-jot)*2.0*cosh(dj*(f.nu+f.m)/temper)/jot/jot*
+	      gsl_sf_bessel_Kn_scaled(2.0,jot);
+	    if (j%2==0) {
+	      pterm*=-1.0;
+	    }
+	    nterm=pterm*tanh(dj*(f.nu+f.m)/temper)*dj/temper;
+	  }
+	}
+    
+	if (inc_antip==false) {
+	  if (j%2==0) {
+	    enterm=(pterm*2.0/tt-pterm/tt/tt*dj-
+		    exp(dj*psi)/2.0/dj*(gsl_sf_bessel_Kn_scaled(1.0,jot)+
+					gsl_sf_bessel_Kn_scaled(3.0,jot)))/f.ms-
+	      pterm*dj*psi_num/temper/temper;
+	  } else {
+	    enterm=(pterm*2.0/tt-pterm/tt/tt*dj+
+		    exp(dj*psi)/2.0/dj*(gsl_sf_bessel_Kn_scaled(1.0,jot)+
+					gsl_sf_bessel_Kn_scaled(3.0,jot)))/f.ms-
+	      pterm*dj*psi_num/temper/temper;
+	  }
+	} else {
+	  double nu2=f.nu;
+	  if (f.inc_rest_mass==false) nu2+=f.m;
+	  if (j%2==0) {
+	    enterm=(pterm*2.0/tt-cosh(dj*nu2/temper)/dj*exp(-jot)*
+		    (gsl_sf_bessel_Kn_scaled(1.0,jot)+
+		     gsl_sf_bessel_Kn_scaled(3.0,jot))+2.0*pterm*nu2*dj/tt/tt*
+		    tanh(dj*nu2/temper)/f.ms)/f.ms;
+	  } else {
+	    enterm=(pterm*2.0/tt+cosh(dj*nu2/temper)/dj*exp(-jot)*
+		    (gsl_sf_bessel_Kn_scaled(1.0,jot)+
+		     gsl_sf_bessel_Kn_scaled(3.0,jot))+2.0*pterm*nu2*dj/tt/tt*
+		    tanh(dj*nu2/temper)/f.ms)/f.ms;
+	  }
+	}
+
+	if (j==1) first_term=pterm;
+	f.pr+=pterm;
+	f.n+=nterm;
+	f.en+=enterm;
+
+	// If the first term is zero, then the rest of the terms
+	// will be zero so just return early
+	if (first_term==0.0) {
+	  f.pr=0.0;
+	  f.n=0.0;
+	  f.ed=0.0;
+	  f.en=0.0;
+	  return true;
+	}
+
+	// Stop if the last term is sufficiently small compared to
+	// the first term
+	if (j>1 && fabs(pterm)<prec*fabs(first_term)) {
+	  f.pr*=prefac;
+	  f.n*=prefac;
+	  f.en*=prefac;
+	  f.ed=-f.pr+f.nu*f.n+temper*f.en;
+	  return true;
+	}
+
+	// End of 'for(size_t j=1;j<=max_term;j++)'
+      }
+
+      // We failed to add enough terms, so return false
+      return false;
+    }
+    
 #ifndef DOXYGEN_NO_O2NS
 
   protected:
