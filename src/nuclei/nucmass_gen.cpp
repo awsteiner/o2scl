4b3722b5233dbcb20c39f45a37ac8c27148e109b
awsteiner <awsteiner@mykolab.com>
2019-06-20 18:21:45 -0400

    Forgot to add nucmass_gen files.

---
+++ src/nuclei/nucmass_gen.cpp
@@ -0,0 +1,115 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2012-2019, Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+#include <o2scl/nucmass_gen.h>
+#include <o2scl/hdf_file.h>
+#include <o2scl/hdf_io.h>
+#include <o2scl/hdf_nucmass_io.h>
+
+using namespace std;
+using namespace o2scl;
+using namespace o2scl_const;
+
+nucmass_gen::nucmass_gen() {
+}
+
+int nucmass_gen::load_be(std::string fname, std::string be_col,
+			 double be_units, bool external) {
+  n=0;
+  
+  std::string dir=o2scl::o2scl_settings.get_data_dir();
+  if (!external) {
+    fname=dir+fname;
+  }
+  
+  o2scl_hdf::hdf_file hf;
+  hf.open(fname);
+  string name;
+  hdf_input(hf,data,name);
+  hf.close();
+
+  if (data.is_column("N")==false) {
+    data.function_column("A-Z","N");
+  }
+  data.sort_table("Z");
+
+  data.new_column("mex");
+  for(size_t i=0;i<n;i++) {
+    double be=data.get(be_col,i);
+    double dZ=data.get("Z",i);
+    double dN=data.get("N",i);
+    double mex=be-(dZ+dN)*m_amu+dZ*m_elec+dN*m_neut+dZ*m_prot;
+    data.set("mex",i,mex);
+  }
+  mex_col_ix=data.lookup_column("mex");
+  
+  n=data.get_nlines();
+  last=n/2;
+
+  return 0;
+}
+
+nucmass_gen::~nucmass_gen() {
+}
+
+bool nucmass_gen::is_included(int l_Z, int l_N) {
+
+  for(size_t i=0;i<n;i++) {
+    if (fabs(data.get("Z",i)-l_Z)<1.0e-12 &&
+	fabs(data.get("N",i)-l_N)<1.0e-12) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+double nucmass_gen::mass_excess(int l_Z, int l_N) {
+  for(size_t i=0;i<n;i++) {
+    if (fabs(data.get("Z",i)-l_Z)<1.0e-12 &&
+	fabs(data.get("N",i)-l_N)<1.0e-12) {
+      return data.get(mex_col_ix,i);
+    }
+  }
+  
+  O2SCL_ERR((((string)"Nucleus with Z=")+itos(l_Z)+" and N="+itos(l_N)+
+	     " not found in nucmass_gen::mass_excess().").c_str(),
+	    exc_enotfound);
+  
+  return 0.0;
+}
+
+double nucmass_gen::get_string(int l_Z, int l_N, std::string column) {
+  for(size_t i=0;i<n;i++) {
+    if (fabs(data.get("Z",i)-l_Z)<1.0e-12 &&
+	fabs(data.get("N",i)-l_N)<1.0e-12) {
+      return data.get(column,i);
+    }
+  }
+  
+  O2SCL_ERR((((string)"Nucleus with Z=")+itos(l_Z)+" and N="+itos(l_N)+
+	     " not found in nucmass_gen::mass_excess().").c_str(),
+	    exc_enotfound);
+  
+  return 0.0;
+}
+
---
+++ src/nuclei/nucmass_gen.h
@@ -0,0 +1,95 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2014-2019, Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+#ifndef O2SCL_NUCMASS_GEN_H
+#define O2SCL_NUCMASS_GEN_H
+
+/** \file nucmass_gen.h
+    \brief File defining \ref o2scl::nucmass_gen
+*/
+
+#include <cmath>
+#include <string>
+#include <map>
+#include <o2scl/nucmass.h>
+
+#ifndef DOXYGEN_NO_O2NS
+namespace o2scl {
+#endif
+
+  /** \brief Nuclear properties 
+   */
+  class nucmass_gen : public nucmass_table {
+    
+  public:
+    
+    nucmass_gen();
+
+    ~nucmass_gen();
+
+    /** \brief Load a file with binding energies
+     */
+    int load_be(std::string fname, std::string be_col,
+		double be_units, bool external=false);
+    
+    /// Return the type, \c "nucmass_gen".
+    virtual const char *type() { return "nucmass_gen"; }
+
+    /// Returns true if data has been loaded
+    bool is_loaded() { return (n>0); }
+
+    /** \brief Return false if the mass formula does not include 
+	specified nucleus
+    */
+    virtual bool is_included(int Z, int N);
+
+    /// Return number of entries
+    virtual size_t get_nentries() { return n; }
+    
+    /// Given \c Z and \c N, return the mass excess in MeV
+    virtual double mass_excess(int Z, int N);
+    
+    /// Given \c Z and \c N, return the mass excess in MeV
+    virtual double get_string(int Z, int N, std::string column);
+    
+#ifndef DOXYGEN_INTERNAL
+
+  protected:
+
+    /// The \ref o2scl::table object containing the data
+    o2scl::table<> data;
+
+    /// Column which refers to the mass excess
+    size_t mex_col_ix;
+    
+    /// The last table index for caching
+    int last;
+    
+#endif
+
+  };
+  
+#ifndef DOXYGEN_NO_O2NS
+}
+#endif
+
+#endif
