a235f4ee4a475ed0da1ed5aadd278eff5dff4bf4
awsteiner <awsteiner@mykolab.com>
2019-06-27 08:46:57 -0400

    Creating pyfix2.

--- auto_refresh.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if git pull | grep -q "Already up to date"; then
-    echo "auto_refresh.sh: No need to update."
-else
-    git pull
-    sudo make install > auto_refresh.out 2> auto_refresh.err
-fi
