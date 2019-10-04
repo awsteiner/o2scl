c1060ab87d11e09bcf2aeda04ec9f9a31ea5006f
Andrew W. Steiner <awsteiner@mykolab.com>
2019-10-04 10:14:10 -0400

    Playing with multiprecision.

--- src/inte/inte_gauss56_cern.h
+++ src/inte/inte_gauss56_cern.h
@@ -26,9 +26,13 @@
 #ifndef O2SCL_CERN_GAUSS56_H
 #define O2SCL_CERN_GAUSS56_H
 
+#ifdef O2SCL_LD_TYPES
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#endif
+
 #include <o2scl/inte.h>
 #include <o2scl/funct.h>
- 
+
 #ifndef DOXYGEN_NO_O2NS
 namespace o2scl {
 #endif
@@ -141,7 +145,126 @@ namespace o2scl {
     }
   
   };
+
+#ifdef O2SCL_LD_TYPES
+  
+  class inte_gauss56_coeffs_float128 {
+
+  public:
+
+    /** \brief Fifth order integration abscissas for 
+	\ref o2scl::inte_gauss56_cern in __float128 precision
+    */
+    __float128 x5[5];
+  
+    /** \brief Fifth order integration weights for 
+	\ref o2scl::inte_gauss56_cern in __float128 precision
+    */
+    __float128 w5[5];
+  
+    /** \brief Sixth order integration abscissas for 
+	\ref o2scl::inte_gauss56_cern in __float128 precision
+    */
+    __float128 x6[6];
+  
+    /** \brief Sixth order integration weights for 
+	\ref o2scl::inte_gauss56_cern in __float128 precision
+    */
+    __float128 w6[6];
+
+    inte_gauss56_coeffs_float128() {
     
+      x5[0]=0.04691007703066800360118656085030352L;
+      x5[1]=0.23076534494715845448184278964989560L;
+      x5[2]=0.5L;
+      x5[3]=0.76923465505284154551815721035010440L;
+      x5[4]=0.95308992296933199639881343914969648L;
+  
+      w5[0]=0.11846344252809454375713202035995868L;
+      w5[1]=0.23931433524968323402064575741781910L;
+      w5[2]=0.28444444444444444444444444444444444L;
+      w5[3]=0.23931433524968323402064575741781910L;
+      w5[4]=0.11846344252809454375713202035995868L;
+  
+      x6[0]=0.03376524289842398609384922275300270L;
+      x6[1]=0.16939530676686774316930020249004733L;
+      x6[2]=0.38069040695840154568474913915964403L;
+      x6[3]=0.61930959304159845431525086084035597L;
+      x6[4]=0.83060469323313225683069979750995267L;
+      x6[5]=0.96623475710157601390615077724699730L;
+  
+      w6[0]=0.08566224618958517252014807108636645L;
+      w6[1]=0.18038078652406930378491675691885806L;
+      w6[2]=0.23395696728634552369493517199477550L;
+      w6[3]=0.23395696728634552369493517199477550L;
+      w6[4]=0.18038078652406930378491675691885806L;
+      w6[5]=0.08566224618958517252014807108636645L;
+
+    }
+  
+  };
+
+  typedef boost::multiprecision::number<
+    boost::multiprecision::cpp_dec_float<50> > cpp_dec_float_50;
+  
+  class inte_gauss56_coeffs_cpp_dec_float_50 {
+
+  public:
+
+    /** \brief Fifth order integration abscissas for 
+	\ref o2scl::inte_gauss56_cern in cpp_dec_float_50 precision
+    */
+    cpp_dec_float_50 x5[5];
+  
+    /** \brief Fifth order integration weights for 
+	\ref o2scl::inte_gauss56_cern in cpp_dec_float_50 precision
+    */
+    cpp_dec_float_50 w5[5];
+  
+    /** \brief Sixth order integration abscissas for 
+	\ref o2scl::inte_gauss56_cern in cpp_dec_float_50 precision
+    */
+    cpp_dec_float_50 x6[6];
+  
+    /** \brief Sixth order integration weights for 
+	\ref o2scl::inte_gauss56_cern in cpp_dec_float_50 precision
+    */
+    cpp_dec_float_50 w6[6];
+
+    inte_gauss56_coeffs_cpp_dec_float_50() {
+    
+      x5[0]=0.04691007703066800360118656085030352L;
+      x5[1]=0.23076534494715845448184278964989560L;
+      x5[2]=0.5L;
+      x5[3]=0.76923465505284154551815721035010440L;
+      x5[4]=0.95308992296933199639881343914969648L;
+  
+      w5[0]=0.11846344252809454375713202035995868L;
+      w5[1]=0.23931433524968323402064575741781910L;
+      w5[2]=0.28444444444444444444444444444444444L;
+      w5[3]=0.23931433524968323402064575741781910L;
+      w5[4]=0.11846344252809454375713202035995868L;
+  
+      x6[0]=0.03376524289842398609384922275300270L;
+      x6[1]=0.16939530676686774316930020249004733L;
+      x6[2]=0.38069040695840154568474913915964403L;
+      x6[3]=0.61930959304159845431525086084035597L;
+      x6[4]=0.83060469323313225683069979750995267L;
+      x6[5]=0.96623475710157601390615077724699730L;
+  
+      w6[0]=0.08566224618958517252014807108636645L;
+      w6[1]=0.18038078652406930378491675691885806L;
+      w6[2]=0.23395696728634552369493517199477550L;
+      w6[3]=0.23395696728634552369493517199477550L;
+      w6[4]=0.18038078652406930378491675691885806L;
+      w6[5]=0.08566224618958517252014807108636645L;
+
+    }
+  
+  };
+
+#endif
+  
   /** \brief 5,6-point Gaussian quadrature (CERNLIB)
       
       If \f$ I_5 \f$ is the 5-point approximation, and \f$ I_6 \f$ is the
@@ -193,7 +316,11 @@ namespace o2scl {
     ytmp=func(a+rang*x6[5]);
     e6+=w6[5]*ytmp;
     res=(e6+e5)*rang/2.0;
-    err=std::abs((e6-e5)*rang);
+    
+    // AWS 10/4/19: I replaced std::abs here with a
+    // more type-agnostic code
+    err=(e6-e5)*rang;
+    if (err<0.0) err=-err;
 
     return success;
   }
--- src/inte/inte_gauss56_cern_ts.cpp
+++ src/inte/inte_gauss56_cern_ts.cpp
@@ -30,14 +30,44 @@ using namespace o2scl;
 double testfun(double tx, double &pa);
 
 double testfun(double tx, double &pa) {
-  double a=pa;
-  return (a*sin(tx)/(tx+0.01));
+  return (pa*sin(tx)/(tx+0.01));
 }
 
 long double testfun_ld(long double tx, long double &pa) {
-  long double a=pa;
-  return (a*sin(tx)/(tx+0.01));
+  return (pa*sinl(tx)/(tx+0.01L));
 }
+
+#ifdef O2SCL_LD_TYPES
+__float128 testfun_f128(__float128 tx, __float128 &pa) {
+  return (pa*sinl(tx)/(tx+0.01));
+}
+
+cpp_dec_float_50 testfun_cdf(cpp_dec_float_50 tx, cpp_dec_float_50 &pa) {
+  return (pa*sin(tx)/(tx+0.01));
+}
+
+/*
+
+// based on
+// https://gcc.gnu.org/onlinedocs/libquadmath/quadmath_005fsnprintf.html
+std::string f128_to_s(const __float128 &x) {
+  char buf[128];
+  int width=46;
+  int n=quadmath_snprintf(buf,128,"%Qa",x);
+  if (n>=128 || n<=0) {
+    cout << "Fail." << endl;
+    exit(-1);
+  }
+  std::string s=buf;
+  return s;
+}
+
+*/
+
+typedef std::function<__float128(__float128)> funct_f128;
+typedef std::function<cpp_dec_float_50(cpp_dec_float_50)> funct_cdf;
+
+#endif
 
 int main(void) {
   cout.setf(ios::scientific);
@@ -63,15 +93,47 @@ int main(void) {
   
   inte_gauss56_cern<funct_ld,long double,
 		    inte_gauss56_coeffs_long_double> cg_ld;
-  long double a_ld=3.0, calc_ld, exact_ld, diff_ld;
+  long double a_ld=3.0L, calc_ld, exact_ld, diff_ld;
 
   funct_ld tf_ld=std::bind(testfun_ld,std::placeholders::_1,a_ld);
   
-  calc_ld=cg_ld.integ(tf_ld,0.0,1.0);
-  exact_ld=a_ld*(0.900729064796877177);
+  calc_ld=cg_ld.integ(tf_ld,0.0L,1.0L);
+  exact_ld=a_ld*(0.900729064796877177036268L);
   t.test_rel<long double>(calc_ld,exact_ld,1.0e-2L,"inte_gauss56_cern ls");
-  diff_ld=fabs(calc_ld-exact_ld);
+  diff_ld=fabsl(calc_ld-exact_ld);
   cout << calc_ld << " " << exact_ld << " " << diff_ld << endl;
+
+  // ------------------------------------------------------
+
+  inte_gauss56_cern<funct_f128,__float128,
+		    inte_gauss56_coeffs_float128> cg_f128;
+  __float128 a_f128=3.0L, calc_f128, exact_f128, diff_f128;
+
+  funct_f128 tf_f128=std::bind(testfun_f128,std::placeholders::_1,a_f128);
+  
+  calc_f128=cg_f128.integ(tf_f128,0.0L,1.0L);
+  exact_f128=a_f128*(0.900729064796877177036268L);
+  //t.test_rel<__float128>(calc_f128,exact_f128,1.0e-2L,
+  //"inte_gauss56_cern ls");
+  diff_f128=fabsl(calc_f128-exact_f128);
+  cout << ((long double)calc_f128) << " "
+       << ((long double)exact_f128) << " "
+       << ((long double)diff_f128) << endl;
+  
+  // ------------------------------------------------------
+
+  inte_gauss56_cern<funct_cdf,cpp_dec_float_50,
+		    inte_gauss56_coeffs_cpp_dec_float_50> cg_cdf;
+  cpp_dec_float_50 a_cdf=3.0L, calc_cdf, exact_cdf, diff_cdf;
+
+  funct_cdf tf_cdf=std::bind(testfun_cdf,std::placeholders::_1,a_cdf);
+  
+  calc_cdf=cg_cdf.integ(tf_cdf,0.0L,1.0L);
+  exact_cdf=a_cdf*(0.900729064796877177036268L);
+  //t.test_rel<cpp_dec_float_50>(calc_cdf,exact_cdf,1.0e-2L,
+  //"inte_gauss56_cern ls");
+  diff_cdf=boost::multiprecision::abs(calc_cdf-exact_cdf);
+  cout << calc_cdf << " " << exact_cdf << " " << diff_cdf << endl;
   
   t.report();
   return 0;
