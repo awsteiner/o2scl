10c13c0df4b06479117c7d8c4d5201c0448558ad
Andrew W. Steiner <awsteiner@mykolab.com>
2019-10-08 11:50:15 -0400

    Added inte_kronrod_boost integrator.

---
+++ src/inte/inte_kronrod_boost.h
@@ -0,0 +1,98 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2019 Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+  
+  -------------------------------------------------------------------
+*/
+#ifndef O2SCL_INTE_KRONROD_BOOST_H
+#define O2SCL_INTE_KRONROD_BOOST_H
+
+/** \file inte_kronrod_boost.h
+    \brief File defining BOOST-based integration coefficients, workspace, 
+    and \ref o2scl::inte_kronrod_boost
+*/
+
+#include <cmath>
+
+#include <boost/math/quadrature/gauss_kronrod.hpp>
+
+#include <o2scl/inte.h>
+
+#ifndef DOXYGEN_NO_O2NS
+namespace o2scl {
+#endif
+
+  /** \brief Gauss-Kronrod integration class (Boost)
+
+      The rule parameter should be either 15, 31, 41, 51, or 61. 
+      
+      This class calls the error handler if the
+      error returned by boost is larger than \ref inte::tol_rel .
+
+      \future Figure out what to do with L1norm. The boost
+      documentation claims that "the error estimates provided by the
+      routine are woefully pessimistic" and the integral appears to be
+      correct, but the boost documentation also says "if there is a
+      significant difference between this [the L1 norm] and the
+      returned value, then the result is likely to be
+      ill-conditioned". It would be nice to test L1 norm in some
+      reasonable way.
+   */
+  template<class func_t=funct, size_t rule=15, class fp_t=double>
+  class inte_kronrod_boost :
+    public inte<func_t> {
+    
+  protected:
+
+    /// Maximum depth
+    size_t max_depth;
+
+    // L1 norm
+    double L1norm;
+    
+  public:
+
+    inte_kronrod_boost() {
+      max_depth=15;
+    }
+  
+    virtual ~inte_kronrod_boost() {
+    }
+    
+    /** \brief Integrate function \c func from \c a to \c b and place
+	the result in \c res and the error in \c err
+    */
+    virtual int integ_err(func_t &func, fp_t a, fp_t b, 
+			  fp_t &res, fp_t &err) {
+      res=boost::math::quadrature::gauss_kronrod<fp_t,rule>::integrate
+	(func,a,b,max_depth,this->tol_rel,&err,&L1norm);
+      if (err>this->tol_rel) {
+	O2SCL_ERR2("Failed to achieve tolerance in ",
+		   "inte_kronrod_boost::integ_err().",o2scl::exc_efailed);
+      }
+      return 0;
+    }
+  
+  };
+  
+#ifndef DOXYGEN_NO_O2NS
+}
+#endif
+
+#endif
---
+++ src/inte/inte_kronrod_boost_ts.cpp
@@ -0,0 +1,57 @@
+/*
+  -------------------------------------------------------------------
+  
+  Copyright (C) 2006-2019, Jerry Gagelman and Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  -------------------------------------------------------------------
+*/
+#include <o2scl/test_mgr.h>
+#include <o2scl/funct.h>
+#include <o2scl/inte_kronrod_boost.h>
+
+using namespace std;
+using namespace o2scl;
+
+// This function oscillates quite rapidly near x=0
+double test_func_1(double x) {
+  return -sin(1.0/(x+0.01))*pow(x+0.01,-2.0);
+}
+
+int main(void) {
+  test_mgr t;
+  t.set_output_level(1);
+
+  inte_kronrod_boost<funct,61> ikb;
+
+  double ans, exact, err;
+
+  cout.setf(ios::scientific);
+
+  funct tf2=test_func_1;
+
+  // Compare with the exact result
+  ikb.integ_err(tf2,0.0,1.0,ans,err);
+  exact=cos(100.0)-cos(1/1.01);
+  std::cout << ans << " " << err << std::endl;
+  t.test_rel(ans,exact,1.0e-8,"qag test");
+
+
+  t.report();
+  return 0;
+}
+
