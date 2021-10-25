00d56361e142c06efe4793d601ba6517e8d41c3f
Andrew W. Steiner <awsteiner@mykolab.com>
2021-10-24 23:31:54 -0400

    Fixing regression on ex_fptr.

--- examples/ex_fptr.h
+++ examples/ex_fptr.h
@@ -7,7 +7,7 @@ int write_file(double x1) {
   fout.open("ex_fptr.out");
   fout << "x y" << endl;
   for(double x=-1.0;x<=2.00001;x+=0.001) {
-    fout << x << " " << c.member_function_parameter(x,p) << endl;
+    fout << x << " " << c.member_function(x,p) << endl;
   }
   fout.close();
   return 0;
