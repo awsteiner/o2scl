29e3526d4ce4f74b420c613615407a1ba48fe33a
Andrew W. Steiner <awsteiner@mykolab.com>
2021-11-18 00:10:23 -0500

    Updating polylog_ts.

--- src/other/polylog_ts.cpp
+++ src/other/polylog_ts.cpp
@@ -23,6 +23,7 @@
 #include <o2scl/test_mgr.h>
 #include <o2scl/inte_adapt_cern.h>
 #include <o2scl/polylog.h>
+#include <o2scl/Li.h>
 
 #ifdef O2SCL_LD_TYPES
 #include <boost/multiprecision/number.hpp>
@@ -76,6 +77,8 @@ int main(void) {
   // Compare polylog values with hard-coded values
   polylog<> p;
   t.test_rel(p.calc(2,-0.5),-0.448414206923646,4.0e-15,"pl 1");
+  t.test_rel(p.calc(2,-0.5),
+             polylogarithm::Li(2,-0.5).real(),4.0e-15,"pl 1b");
   t.test_rel(p.calc(2,-2.0),-1.43674636688368,4.0e-15,"pl 2");
   t.test_rel(p.calc(3,-0.5),-0.472597844658897,4.0e-15,"pl 3");
   t.test_rel(p.calc(3,-2.0),-1.66828336396657,4.0e-15,"pl 4");
