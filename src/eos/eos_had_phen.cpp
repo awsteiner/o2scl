bc504b323469cda62a478aab923650c17414b04d
Andrew W. Steiner <awsteiner@mykolab.com>
2021-12-07 21:06:46 -0500

    Fixing warning of vector sizes.

--- src/eos/eos_had_phen.cpp
+++ src/eos/eos_had_phen.cpp
@@ -1171,8 +1171,8 @@ double eos_had_phen::free_energy_density_detail
     tx.line_of_names("nb ed mu");
     for(nb=0.08;nb<2.0;nb+=0.01) {
       new_ns_eos(nb,n,e_ns,densdnn);
-      double line[3]={nb,e_ns,densdnn};
-      tx.line_of_data(3,line);
+      vector<double> line={nb,e_ns,densdnn};
+      tx.line_of_data(line.size(),line);
     }
 
     tx.function_column("ed+939.0/197.33*nb","edf");
@@ -2959,8 +2959,8 @@ int eos_had_phen::vir_comp(std::vector<std::string> &sv, bool itive_com) {
 	double F_vir=free_energy_density_virial(neutron,proton,T,th2)/
 	  nb*hc_mev_fm;
 	double zn=exp(neutron.mu/T);
-	double line2[3]={log10(nb),zn,F_vir};
-	t2.line_of_data(3,line2);
+	vector<double> line2={log10(nb),zn,F_vir};
+	t2.line_of_data(line2.size(),line2);
       }
     }
   }
