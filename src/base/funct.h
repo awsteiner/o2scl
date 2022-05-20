71a96f5927947da97946c7c7da6da0ebb00b4713
Andrew W. Steiner <awsteiner@mykolab.com>
2022-05-20 00:13:56 -0400

    Work on funct_ret objects.

--- src/base/funct.h
+++ src/base/funct.h
@@ -386,12 +386,13 @@ namespace o2scl {
 
 #endif
 
-  /** \brief Desc
+  /** \brief A wrapper to provide a template operator() function
+      from a set of type-specific function objects
    */
   class funct_multip_wrapper {
-  
-  public:
-  
+
+  protected:
+    
     funct f;
     funct_ld f_ld;
     funct_cdf25 f_cdf25;
@@ -399,6 +400,58 @@ namespace o2scl {
     funct_cdf50 f_cdf50;
     funct_cdf100 f_cdf100;
   
+    /** \brief The function for double
+     */
+    double eval(double x) {
+      return f(x);
+    }
+  
+    /** \brief The function for long double
+     */
+    long double eval(long double x) {
+      return f_ld(x);
+    }
+
+    /** \brief The function for 25-digit numbers
+     */
+    boost::multiprecision::number<
+      boost::multiprecision::cpp_dec_float<25>> eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<25>> x) {
+      return f_cdf25(x);
+    }
+  
+    /** \brief The function for 35-digit numbers
+     */
+    boost::multiprecision::number<
+      boost::multiprecision::cpp_dec_float<35>> eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<35>> x) {
+      return f_cdf35(x);
+    }
+  
+    /** \brief The function for 50-digit numbers
+     */
+    boost::multiprecision::number<
+      boost::multiprecision::cpp_dec_float<50>> eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<50>> x) {
+      return f_cdf50(x);
+    }
+  
+    /** \brief The function for 100-digit numbers
+     */
+    boost::multiprecision::number<
+      boost::multiprecision::cpp_dec_float<100>> eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<100>> x) {
+      return f_cdf100(x);
+    }
+  
+  public:
+
+    /** \brief Create a wrapper from the given function objects
+    */
     funct_multip_wrapper(funct &f_x,
                          funct_ld &f_ld_x,
                          funct_cdf25 &f_cdf25_x,
@@ -414,55 +467,7 @@ namespace o2scl {
       return;
     }
 
-    /** \brief Desc
-     */
-    double eval(double x) {
-      return f(x);
-    }
-  
-    /** \brief Desc
-     */
-    long double eval(long double x) {
-      return f_ld(x);
-    }
-
-    /** \brief Desc
-     */
-    boost::multiprecision::number<
-      boost::multiprecision::cpp_dec_float<25>> eval
-    (boost::multiprecision::number<
-     boost::multiprecision::cpp_dec_float<25>> x) {
-      return f_cdf25(x);
-    }
-  
-    /** \brief Desc
-     */
-    boost::multiprecision::number<
-      boost::multiprecision::cpp_dec_float<35>> eval
-    (boost::multiprecision::number<
-     boost::multiprecision::cpp_dec_float<35>> x) {
-      return f_cdf35(x);
-    }
-  
-    /** \brief Desc
-     */
-    boost::multiprecision::number<
-      boost::multiprecision::cpp_dec_float<50>> eval
-    (boost::multiprecision::number<
-     boost::multiprecision::cpp_dec_float<50>> x) {
-      return f_cdf50(x);
-    }
-  
-    /** \brief Desc
-     */
-    boost::multiprecision::number<
-      boost::multiprecision::cpp_dec_float<100>> eval
-    (boost::multiprecision::number<
-     boost::multiprecision::cpp_dec_float<100>> x) {
-      return f_cdf100(x);
-    }
-  
-    /** \brief Desc
+    /** \brief The template operator() function
      */
     template<class fp_t> fp_t operator()(fp_t x) {
       return eval(x);
@@ -498,22 +503,24 @@ namespace o2scl {
 
   public:
 
-    /** \brief Desc
+    /** \brief Create a function evaluator based on the operator()
+        given in the class \c fx of type \c func_t
      */
     funct_multip(func_t &fx) : f(fx) {
       verbose=0;
       tol_rel=-1.0;
     }
 
-    /** \brief Desc
+    /** \brief Verbosity parameter
      */
     int verbose;
 
-    /** \brief Desc
+    /** \brief Relative tolerance
      */
     double tol_rel;
 
-    /** \brief Desc
+    /** \brief Evalulate the function with the specified tolerance
+        and provide an error estimate
      */
     template<class fp_t> int eval_tol_err(fp_t x, fp_t &val,
                                           fp_t &err, double tol_loc=-1.0) {
@@ -641,17 +648,336 @@ namespace o2scl {
       return o2scl::exc_efailed;
     }
 
-    /** \brief Desc
+    /** \brief Evaluate the function and return the error estimate
+        with the default tolerance for the specified type
      */
     template<class fp_t> int eval_err(fp_t x, fp_t &val, fp_t &err) {
       return eval_tol_err(x,val,err);
     }
   
-    /** \brief Desc
+    /** \brief Evalulate the function without an error estimate
      */
     template<class fp_t> fp_t operator()(fp_t x) {
       fp_t val,err;
+      
+      eval_err(x,val,err);
     
+      return val;
+    }
+    
+  };
+
+  /// One-dimensional function typedef in src/base/funct.h
+  typedef std::function<int(double,double &)> funct_ret;
+
+  typedef std::function<int(long double,long double &)> funct_ret_ld;
+
+  typedef std::function<int(boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<25> >,
+                            boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<25> > &)>
+  funct_ret_cdf25;
+  
+  typedef std::function<int(boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<35> >,
+                            boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<35> > &)>
+  funct_ret_cdf35;
+  
+  typedef std::function<int(boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<50> >,
+                            boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<50> > &)>
+  funct_ret_cdf50;
+  
+  typedef std::function<int(boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<100> >,
+                            boost::multiprecision::number<
+                            boost::multiprecision::cpp_dec_float<100> > &)>
+  funct_ret_cdf100;
+
+  /** \brief A wrapper to provide a template operator() function
+      from a set of type-specific function objects
+   */
+  class funct_ret_multip_wrapper {
+
+  protected:
+    
+    funct_ret f;
+    funct_ret_ld f_ld;
+    funct_ret_cdf25 f_cdf25;
+    funct_ret_cdf35 f_cdf35;
+    funct_ret_cdf50 f_cdf50;
+    funct_ret_cdf100 f_cdf100;
+  
+    /** \brief The function for double
+     */
+    int eval(double x, double &y) {
+      return f(x,y);
+    }
+  
+    /** \brief The function for long double
+     */
+    int eval(long double x, long double &y) {
+      return f_ld(x,y);
+    }
+
+    /** \brief The function for 25-digit numbers
+     */
+    int eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<25>> x,
+     boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<25>> &y) {
+      return f_cdf25(x,y);
+    }
+  
+    /** \brief The function for 35-digit numbers
+     */
+    int eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<35>> x,
+     boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<35>> &y) {
+      return f_cdf35(x,y);
+    }
+    
+    /** \brief The function for 50-digit numbers
+     */
+    int eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<50>> x,
+     boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<50>> &y) {
+      return f_cdf50(x,y);
+    }
+  
+    /** \brief The function for 100-digit numbers
+     */
+    int eval
+    (boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<100>> x,
+     boost::multiprecision::number<
+     boost::multiprecision::cpp_dec_float<100>> &y) {
+      return f_cdf100(x,y);
+    }
+  
+  public:
+
+    /** \brief Create a wrapper from the given function objects
+    */
+    funct_ret_multip_wrapper(funct_ret &f_x,
+                             funct_ret_ld &f_ld_x,
+                             funct_ret_cdf25 &f_cdf25_x,
+                             funct_ret_cdf35 &f_cdf35_x,
+                             funct_ret_cdf50 &f_cdf50_x,
+                             funct_ret_cdf100 &f_cdf100_x) {
+      f=f_x;
+      f_ld=f_ld_x;
+      f_cdf25=f_cdf25_x;
+      f_cdf35=f_cdf35_x;
+      f_cdf50=f_cdf50_x;
+      f_cdf100=f_cdf100_x;
+      return;
+    }
+
+    /** \brief The template operator() function
+     */
+    template<class fp_t> int operator()(fp_t x, fp_t &y) {
+      return eval(x,y);
+    }
+
+  };
+
+  /** \brief Use multiprecision to automatically evaluate a function to
+      a specified level of precision
+  */
+  template<class func_t=funct_ret_multip_wrapper> class funct_ret_multip {
+  
+  protected:
+
+    /** \brief Desc
+     */
+    func_t &f;
+
+    /// \name Typedefs for multiprecision types
+    //@{
+    typedef
+    boost::multiprecision::number<boost::multiprecision::cpp_dec_float<25> >
+    cpp_dec_float_25;
+  
+    typedef
+    boost::multiprecision::number<boost::multiprecision::cpp_dec_float<35> >
+    cpp_dec_float_35;
+  
+    typedef boost::multiprecision::cpp_dec_float_50 cpp_dec_float_50;
+  
+    typedef boost::multiprecision::cpp_dec_float_100 cpp_dec_float_100;
+    //@}
+
+  public:
+
+    /** \brief Create a function evaluator based on the operator()
+        given in the class \c fx of type \c func_t
+     */
+    funct_ret_multip(func_t &fx) : f(fx) {
+      verbose=0;
+      tol_rel=-1.0;
+    }
+
+    /** \brief Verbosity parameter
+     */
+    int verbose;
+
+    /** \brief Relative tolerance
+     */
+    double tol_rel;
+
+    /** \brief Evalulate the function with the specified tolerance
+        and provide an error estimate
+     */
+    template<class fp_t> int eval_tol_err(fp_t x, fp_t &val,
+                                          fp_t &err, double tol_loc=-1.0) {
+      
+      if (tol_loc<=0.0 && tol_rel<=0.0) {
+        tol_loc=pow(10.0,-std::numeric_limits<fp_t>::digits10);
+      } else {
+        tol_loc=tol_rel;
+      }
+      if (verbose>0) {
+        std::cout << "Set tol to: " << tol_loc << std::endl;
+      }
+    
+      double x_d=static_cast<double>(x), y_d;
+      long double x_ld=static_cast<long double>(x), y_ld;
+      int r_d, r_ld;
+      r_d=f(x_d,y_d);
+      r_ld=f(x_ld,y_ld);
+
+      if (r_d==0 && r_ld==0 && y_ld==0 && y_d==0) {
+        val=0;
+        err=0;
+        return 0;
+      }
+      if (r_d==0 && r_ld==0 && y_ld!=0) {
+        err=static_cast<fp_t>(abs(y_ld-y_d)/abs(y_ld));
+        if (err<tol_loc) {
+          val=static_cast<fp_t>(y_ld);
+          return 0;
+        }
+      }
+      if (verbose>0) {
+        std::cout << "Failed 1: " << r_ld << " " << r_d << " "
+                  << dtos(y_ld,0) << " " << dtos(y_d,0) << " "
+                  << dtos(err,0) << " " << tol_loc << std::endl;
+      }
+    
+      cpp_dec_float_25 x_cdf25=static_cast<cpp_dec_float_25>(x);
+      cpp_dec_float_25 y_cdf25;
+      int r_cdf25=f(x_cdf25,y_cdf25);
+
+      if (r_cdf25==0 && r_ld==0 && y_cdf25==0 && y_ld==0) {
+        val=0;
+        err=0;
+        return 0;
+      }
+      if (r_cdf25==0 && r_ld==0 && y_cdf25!=0) {
+        err=static_cast<fp_t>(abs(y_cdf25-y_ld)/abs(y_cdf25));
+        if (err<tol_loc) {
+          val=static_cast<fp_t>(y_cdf25);
+          return 0;
+        }
+      }
+      if (verbose>0) {
+        std::cout << "Failed 2: " << r_cdf25 << " " << r_ld << " "
+                  << dtos(y_cdf25,0) << " " << dtos(y_ld,0) << " "
+                  << dtos(err,0) << " " << tol_loc << std::endl;
+      }
+    
+      cpp_dec_float_35 x_cdf35=static_cast<cpp_dec_float_35>(x);
+      cpp_dec_float_35 y_cdf35;
+      int r_cdf35=f(x_cdf35,y_cdf35);
+        
+      if (r_cdf35==0 && r_cdf25==0 && y_cdf35==0 && y_cdf25==0) {
+        val=0;
+        err=0;
+        return 0;
+      }
+      if (r_cdf35==0 && r_cdf25==0 && y_cdf35!=0) {
+        err=static_cast<fp_t>(abs(y_cdf35-y_cdf25)/abs(y_cdf35));
+        if (err<tol_loc) {
+          val=static_cast<fp_t>(y_cdf35);
+          return 0;
+        }
+      }
+      if (verbose>0) {
+        std::cout << "Failed 3: " << r_cdf35 << " " << r_cdf25 << " "
+                  << dtos(y_cdf35,0) << " " << dtos(y_cdf25,0) << " "
+                  << dtos(err,0) << " " << tol_loc << std::endl;
+      }
+    
+      cpp_dec_float_50 x_cdf50=static_cast<cpp_dec_float_50>(x);
+      cpp_dec_float_50 y_cdf50;
+      int r_cdf50=f(x_cdf50,y_cdf50);
+    
+      if (r_cdf50==0 && r_cdf35==0 && y_cdf50==0 && y_cdf35==0) {
+        val=0;
+        err=0;
+        return 0;
+      }
+      if (r_cdf50==0 && r_cdf35==0 && y_cdf50!=0) {
+        err=static_cast<fp_t>(abs(y_cdf50-y_cdf35)/abs(y_cdf50));
+        if (err<tol_loc) {
+          val=static_cast<fp_t>(y_cdf50);
+          return 0;
+        }
+      }
+      if (verbose>0) {
+        std::cout << "Failed 4: " << r_cdf50 << " " << r_cdf35 << " "
+                  << dtos(y_cdf50,0) << " " << dtos(y_cdf35,0) << " "
+                  << dtos(err,0) << " " << tol_loc << std::endl;
+      }
+    
+      cpp_dec_float_100 x_cdf100=static_cast<cpp_dec_float_100>(x);
+      cpp_dec_float_100 y_cdf100;
+      int r_cdf100=f(x_cdf100,y_cdf100);
+    
+      if (r_cdf100==0 && r_cdf50==0 && y_cdf100==0 && y_cdf50==0) {
+        val=0;
+        err=0;
+        return 0;
+      }
+      if (r_cdf100==0 && r_cdf50==0 && y_cdf100!=0) {
+        err=static_cast<fp_t>(abs(y_cdf100-y_cdf50)/abs(y_cdf100));
+        if (err<tol_loc) {
+          val=static_cast<fp_t>(y_cdf100);
+          return 0;
+        }
+      }
+      if (verbose>0) {
+        std::cout << "Failed 5: " << r_cdf100 << " " << r_cdf50 << " "
+                  << dtos(y_cdf100,0) << " " << dtos(y_cdf50,0) << " "
+                  << dtos(err,0) << " " << tol_loc << std::endl;
+      }
+    
+      O2SCL_ERR2("Failed to compute with requested accuracy ",
+                 "in funct_ret_multip::eval_tol_err().",
+                 o2scl::exc_efailed);
+      return o2scl::exc_efailed;
+    }
+
+    /** \brief Evaluate the function and return the error estimate
+        with the default tolerance for the specified type
+     */
+    template<class fp_t> int eval_err(fp_t x, fp_t &val, fp_t &err) {
+      return eval_tol_err(x,val,err);
+    }
+  
+    /** \brief Evalulate the function without an error estimate
+     */
+    template<class fp_t> fp_t operator()(fp_t x) {
+      fp_t val,err;
+      
       eval_err(x,val,err);
     
       return val;
