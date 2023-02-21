121a89e5afd2fff5392d7a06b2c0ba4eda410593
Andrew W. Steiner <awsteiner@mykolab.com>
2023-02-21 06:02:17 -0500

    Fixed typo in lib_settings.

--- src/base/lib_settings.cpp
+++ src/base/lib_settings.cpp
@@ -644,6 +644,6 @@ std::string lib_settings_class::py_version() {
   oss << PY_MAJOR_VERSION << " "
       << PY_MINOR_VERSION << " " << PY_MICRO_VERSION << " "
       << PY_RELEASE_LEVEL << " " << PY_RELEASE_SERIAL << " "
-      << Py_Version;
+      << PY_VERSION;
   return oss.str();
 }
