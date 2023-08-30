17548e4c379513c0aca0f7fc4bb19cbcb534e944
Andrew W. Steiner <awsteiner@mykolab.com>
2023-08-30 13:52:44 -0400

    Updating constants.

--- src/part/fermion_mag_zerot_ts.cpp
+++ src/part/fermion_mag_zerot_ts.cpp
@@ -38,7 +38,7 @@ int main(void) {
   cout.setf(ios::scientific);
 
   // In "Gaussian units":
-  cout << pow(4.414e13*o2scl_const::ec_gauss_fm2,2.0)*hc_mev_fm/8.0/pi/
+  cout << pow(4.414e13*o2scl_const::ec_gauss_fm2_f<double>(),2.0)*hc_mev_fm/8.0/pi/
     o2scl_const::fine_structure_f<double>() << endl;
 
   fermion_mag_zerot mfz;
@@ -50,39 +50,39 @@ int main(void) {
 
   double mag_field=1.0e16;
 
-  mfz.calc_mu_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_mu_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << " " << e.mu << " " << e.ed << " " << e.pr << endl;
   cout << mfz.nmax_up << " " << mfz.nmax_dn << endl;
 
   e.mu-=e.m;
   e.inc_rest_mass=false;
-  mfz.calc_mu_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_mu_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << " " << e.mu << " " << e.ed+e.n*e.m << " " << e.pr << endl;
   cout << mfz.nmax_up << " " << mfz.nmax_dn << endl;
   e.inc_rest_mass=true;
   e.mu+=e.m;
 
   e.mu*=0.9;
-  mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << " " << e.mu << " " << e.ed << " " << e.pr << endl;
   cout << endl;
 
   e.m=0.5;
   for(e.n=1.0;e.n>0.9e-10;e.n/=10.0) {
-    mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+    mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
     cout << e.n << " " << e.mu << " " << e.ed << " " << e.pr << endl;
   }
   cout << endl;
 
   e.n=pow(0.011,1.5)*sqrt(2.0)/2.0/pi2*(1.0-0.0001);
   cout << e.n << endl;
-  mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << mfz.nmax_up << " " << mfz.nmax_dn << endl;
   // This point doesn't work unless err_nonconv is set to false in
   // the fermion_mag_zerot class. I don't know why.
   e.n=pow(0.011,1.5)*sqrt(2.0)/2.0/pi2*(1.0+0.0001);
   cout << e.n << endl;
-  mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << mfz.nmax_up << " " << mfz.nmax_dn << endl;
   
   cout << endl;
@@ -90,22 +90,22 @@ int main(void) {
   e.inc_rest_mass=false;
   e.m=0.5;
   for(e.n=1.0;e.n>0.9e-8;e.n/=10.0) {
-    mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2,0.0);
+    mfz.calc_density_zerot_mag(e,-mag_field*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
     cout << e.n << " " << e.mu << " " << e.ed+e.n*e.m << " " << e.pr << endl;
   }
 
   cout << endl;
-  cout << o2scl_const::ec_gauss_fm2 << endl;
+  cout << o2scl_const::ec_gauss_fm2_f<double>() << endl;
   e.g=2.0;
   e.m=0.511/o2scl_const::hc_mev_fm;
   e.mu=0.5;
-  mfz.calc_mu_zerot_mag(e,-1.0e18*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_mu_zerot_mag(e,-1.0e18*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << endl;
-  mfz.calc_mu_zerot_mag(e,-1.0e16*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_mu_zerot_mag(e,-1.0e16*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << endl;
-  mfz.calc_mu_zerot_mag(e,-1.0e12*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_mu_zerot_mag(e,-1.0e12*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << endl;
-  mfz.calc_mu_zerot_mag(e,-1.0e9*o2scl_const::ec_gauss_fm2,0.0);
+  mfz.calc_mu_zerot_mag(e,-1.0e9*o2scl_const::ec_gauss_fm2_f<double>(),0.0);
   cout << e.n << endl;
   frel.calc_mu_zerot(e);
   cout << e.n << endl;
