294a70c6df0b399f36b8f29da22ff088d2329525
Andrew W. Steiner <awsteiner@mykolab.com>
2023-10-04 18:01:38 -0400

    More fp_t work in ode and ode_boost draft.

---
+++ src/ode/ode_boost.h
@@ -0,0 +1,96 @@
+/*
+  ───────────────────────────────────────────────────────────────────
+  
+  Copyright (C) 2006-2023, Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  ───────────────────────────────────────────────────────────────────
+*/
+/* ode-initval/rkck.c
+ * 
+ * Copyright (C) 1996, 1997, 1998, 1999, 2000 Gerard Jungman
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 
+ * 02110-1301, USA.
+ */
+#ifndef O2SCL_ODE_BOOST_H
+#define O2SCL_ODE_BOOST_H
+
+/** \file ode_boost.h
+    \brief File defining \ref o2scl::ode_boost 
+*/
+
+#include <o2scl/err_hnd.h>
+#include <o2scl/ode_funct.h>
+#include <o2scl/ode_step.h>
+#include <boost/numeric/odeint.hpp>
+
+namespace o2scl {
+
+  /** \brief Simple ODE stepper from boost
+   */
+  template<class step_t,
+           class vec_y_t=boost::numeric::ublas::vector<double>,
+	   class vec_dydx_t=vec_y_t, class vec_yerr_t=vec_y_t, 
+           class func_t=ode_funct_boost<>, class fp_t=double> class ode_boost :
+    public ode_step<vec_y_t,vec_dydx_t,vec_yerr_t,func_t,fp_t> {
+
+  protected:
+
+    step_t stepper;
+    
+  public:
+
+    ode_boost() {
+    }
+      
+    virtual ~ode_boost() {
+    }
+
+    /** \brief Perform an integration step
+     */
+    virtual int step(fp_t x, fp_t h, size_t n, vec_y_t &y, 
+                     vec_dydx_t &dydx, vec_y_t &yout, vec_yerr_t &yerr, 
+                     vec_dydx_t &dydx_out, func_t &derivs) {
+
+      func_t f(derivs,n);
+      for(size_t j=0;j<n;j++) {
+        yout[j]=y[j];
+      }
+      stepper.do_step(f,yout,dydx,x,h);
+      derivs(x+h,yout,dydx_out);
+    
+      return 0;
+    }
+    
+  };
+  
+}
+
+#endif
---
+++ src/ode/ode_boost_ts.cpp
@@ -0,0 +1,116 @@
+/*
+  ───────────────────────────────────────────────────────────────────
+  
+  Copyright (C) 2006-2023, Andrew W. Steiner
+  
+  This file is part of O2scl.
+  
+  O2scl is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  (at your option) any later version.
+  
+  O2scl is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+  
+  You should have received a copy of the GNU General Public License
+  along with O2scl. If not, see <http://www.gnu.org/licenses/>.
+
+  ───────────────────────────────────────────────────────────────────
+*/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <gsl/gsl_odeiv.h>
+
+#include <boost/numeric/ublas/vector.hpp>
+
+#include <o2scl/test_mgr.h>
+#include <o2scl/funct.h>
+#include <o2scl/ode_funct.h>
+#include <o2scl/ode_rkck_gsl.h>
+#include <o2scl/ode_boost.h>
+
+typedef boost::numeric::ublas::vector<double> ubvector;
+
+int expon(double x, size_t nv, const ubvector &y, ubvector &dydx) {
+  dydx[0]=y[0];
+  return 0;
+}
+
+using namespace std;
+using namespace o2scl;
+
+int main(void) {
+  cout.setf(ios::scientific);
+
+  test_mgr t;
+  t.set_output_level(1);
+
+#ifdef O2SCL_NEVER_DEFINED
+  
+  {
+    double x, dx=1.0e-1, x2;
+    ode_funct of=expon;
+    ode_funct_boost<ubvector,ode_funct,double> ofb(of,1);
+
+    ubvector y(1), dydx(1), yout(1), yerr(1), dydx_out(1);
+    ubvector y2(1), dydx2(1), yout2(1), yerr2(1), dydx_out2(1);
+
+    ode_boost<boost::numeric::odeint::runge_kutta_cash_karp54<ubvector>,
+              ubvector,ubvector,ubvector,
+              ode_funct_boost<ubvector,ode_funct,double>,
+              double> ob;
+    ode_rkck_gsl<> org;
+
+    x=1.0;
+    y[0]=1.0;
+    x2=1.0;
+    y2[0]=1.0;
+
+    cout << "x            y(calc)      y(exact)     diff         yerr"
+	 << "          y'" << endl;
+    cout << 1.0 << " " << 1.0 << " " << 1.0 << " " << 0.0 << " " << -0.0 
+	 << " " << 1.0 << endl;
+
+    // Compute initial derivative
+    expon(x,1,y,dydx);
+    expon(x2,1,y2,dydx2);
+
+    for(size_t i=1;i<=40;i++) {
+
+      // Make a step
+      ob.step(x,dx,1,y,dydx,y,yerr,dydx,of);
+
+      // Output results
+      cout << x+dx << " " << y[0] << " " << exp(x+dx)/exp(1.0) << " "
+	   << fabs(y[0]-exp(x+dx)/exp(1.0)) << " " << yerr[0] << " ";
+      cout << dydx[0] << endl;
+
+      // Make a step
+      org.step(x2,dx,1,y2,dydx2,y2,yerr2,dydx2,of);
+
+      // Output results
+      cout << x2+dx << " " << y2[0] << " " << exp(x2+dx)/exp(1.0) << " "
+	   << fabs(y2[0]-exp(x2+dx)/exp(1.0)) << " " << yerr2[0] << " ";
+      cout << dydx2[0] << endl;
+
+      x+=dx;
+      x2+=dx;
+
+      exit(-1);
+
+    }
+
+  }
+  
+#endif
+
+  t.report();
+
+
+  return 0;
+}
