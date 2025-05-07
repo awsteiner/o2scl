25f20146b6d69f7619392530a4f9e03a42ec1117
Andrew W. Steiner <awsteiner0@protonmail.com>
2025-05-07 15:42:16 -0400

    Updating check scripts.

--- docker/torch_check.sh
+++ docker/torch_check.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+echo "PATH:"
+echo $PATH
+echo ""
+echo "LD_LIBRARY_PATH:"
+echo $LD_LIBRARY_PATH
+echo ""
+echo "Python version:"
 python3 --version
+echo ""
+echo "Output of 'nvidia-smi':"
 nvidia-smi
+echo ""
+echo "NVCC version:"
 nvcc --version
+echo ""
+echo "Torch:"
 python3 -c "import torch; print('CUDA avail:',torch.cuda.is_available()); print('CUDA version:',torch.version.cuda); print('CUDA built:',torch.backends.cuda.is_built()); print('CUDNN version:',torch.backends.cudnn.version()); print('CUDA device name:',torch.cuda.get_device_name(0))"
