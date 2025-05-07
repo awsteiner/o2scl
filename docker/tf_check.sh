e300ddabeb6d2c2677c0128dcae191ae29112365
Andrew W. Steiner <awsteiner0@protonmail.com>
2025-05-07 13:42:43 -0400

    Work on docker images.

--- docker/tf_check.sh
+++ docker/tf_check.sh
@@ -2,6 +2,4 @@
 python3 --version
 nvidia-smi
 nvcc --version
-python3 -c "import tensorflow"
-python3 -c "import tensorflow as tf; print(tf.config.list_physical_devices('GPU'))"
-python -c 'import tensorflow as tf; print(tf.sysconfig.get_build_info())'
+python3 -c "import tensorflow as tf; print('TF GPU devices:',tf.config.list_physical_devices('GPU')); print('TF build info:',tf.sysconfig.get_build_info())"
--- docker/torch_check.sh
+++ docker/torch_check.sh
@@ -2,5 +2,4 @@
 python3 --version
 nvidia-smi
 nvcc --version
-python3 -c "import torch"
-python3 -c "import torch; print(torch.cuda.is_available())"
+python3 -c "import torch; print('CUDA avail:',torch.cuda.is_available()); print('CUDA version:',torch.version.cuda); print('CUDA built:',torch.backends.cuda.is_built()); print('CUDNN version:',torch.backends.cudnn.version()); print('CUDA device name:',torch.cuda.get_device_name(0))"
